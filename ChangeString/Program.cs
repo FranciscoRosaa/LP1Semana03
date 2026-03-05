9d5f14488a041f1561adc4457d4d0bf3e1e03761
Francisco <franciscorosa.work@gmail.com>
2026-03-05 15:08:24 +0000

    Add print 'x' if the character on the string is the same the user chose

---
+++ ChangeString/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string s = Console.ReadLine();
+
+            Console.Write("Char: ");
+            char c = char.Parse(Console.ReadLine());
+
+            string result = "";
+
+            foreach (char l in s)
+            {
+                if (l == c)
+                {
+                    result += "x";
+                }
+                else
+                {
+                    result += l;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
